eb0456c0eb348e204d129261ebfb1153ed5c799d
jackMarshall051 <57306993+jackMarshall051@users.noreply.github.com>
2020-10-05 20:38:44 +1100

    Add files via upload

---
+++ js/contactMe.js
@@ -0,0 +1,12 @@
+function checkKey() {
+  var input = document.getElementById('keyInput').value;
+  var correct = "1234567890";
+   if (correct == input) {
+    window.location.href = "../Live/admin.html"
+  }
+}
+
+
+
+
+//https://lig-membres.imag.fr/donsez/cours/exemplescourstechnoweb/js_securehash/
